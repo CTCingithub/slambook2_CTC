3fecc39694d6e913a14ec0f4fc82c66156b82727
CTC <2801320287@qq.com>
2024-10-19 16:06:31 +0800

    Uploaded on 2024.10.19

---
+++ Chapter02/05_header_file/build/CMakeFiles/useHello.dir/src/useHello.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/useHello.dir/src/useHello.cpp.o: \
+ /home/ctc/GitProjects/slambook2_CTC/Chapter02/05_header_file/src/useHello.cpp \
+ /usr/include/stdc-predef.h \
+ /home/ctc/GitProjects/slambook2_CTC/Chapter02/05_header_file/include/libHelloSLAM.h
---
+++ Chapter02/05_header_file/include/libHelloSLAM.h
@@ -0,0 +1,8 @@
+#ifndef LIBHELLOSLAM_H_
+#define LIBHELLOSLAM_H_
+// 宏定义，防止重复引用头文件，引发重定义错误
+
+// 调用libHelloSLAM.cpp中定义的printHello函数
+void printHello();
+
+#endif
