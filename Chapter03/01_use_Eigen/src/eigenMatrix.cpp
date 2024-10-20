4db73e2600d4ec7ab9a4bb5f7bf0d86a3f5addbf
CTC <2801320287@qq.com>
2024-10-20 13:00:24 +0800

    Updated on 2024.10.20

--- Chapter03/01_use_Eigen/src/eigenMatrix.cpp
+++ Chapter03/01_use_Eigen/src/eigenMatrix.cpp
@@ -55,6 +55,10 @@ int main(int argc, char **argv)
     std::cout << "\nvd_3d:" << std::endl
               << vd_3d << std::endl;
 
+    // 访问矩阵元素
+    std::cout << "\nmatrix_23(0,0): " << matrix_23(0, 0) << std::endl;
+    std::cout << "\nvd_3d(1): " << vd_3d(1) << std::endl;
+
     // 矩阵和向量经过显式变换后相乘
     Eigen::Matrix<double, 2, 1> result_1 = matrix_23.cast<double>() * v_3d;
     Eigen::Matrix<float, 2, 1> result_2 = matrix_23 * vd_3d;
@@ -103,6 +107,30 @@ int main(int argc, char **argv)
               << eigen_solver.eigenvectors() << std::endl;
 
     // 解线性方程
+    std::cout << "\nLinear Equation Solution" << std::endl
+              << "\n===========================================" << std::endl;
+    Eigen::Matrix<double, MATRIX_SIZE, MATRIX_SIZE> matrix_NN = Eigen::MatrixXd::Random(MATRIX_SIZE, MATRIX_SIZE);
+    matrix_NN = matrix_NN * matrix_NN.transpose();
+    Eigen::Matrix<double, MATRIX_SIZE, 1> v_Nd = Eigen::MatrixXd::Random(MATRIX_SIZE, 1);
+
+    // 计时器
+    clock_t time_start = clock();
+    // 直接求逆矩阵求解
+    Eigen::Matrix<double, MATRIX_SIZE, 1> x_Nd = matrix_NN.inverse() * v_Nd;
+    std::cout << "\nDuration in normal inverse case:" << 1000 * (clock() - time_start) / (double)CLOCKS_PER_SEC << "ms" << std::endl;
+    // std::cout << "x_Nd = " << x_Nd.transpose() << std::endl;
+
+    // 重新计时
+    time_start = clock();
+    // 使用QR分解求解
+    x_Nd = matrix_NN.colPivHouseholderQr().solve(v_Nd);
+    std::cout << "\nDuration in QR decomposition case:" << 1000 * (clock() - time_start) / (double)CLOCKS_PER_SEC << "ms" << std::endl;
+
+    // 重新计时
+    time_start = clock();
+    // 使用Cholesky分解求解
+    x_Nd = matrix_NN.ldlt().solve(v_Nd);
+    std::cout << "\nDuration in Cholesky decomposition case:" << 1000 * (clock() - time_start) / (double)CLOCKS_PER_SEC << "ms" << std::endl;
 
     return 0;
 }
