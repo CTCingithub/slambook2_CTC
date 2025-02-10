eef40c36ce97ed1995f1d7ac53e6334e1f7cb6c7
CTCingithub <2801320287@qq.com>
2025-02-10 16:41:19 +0800

    Updated on 2025.02.10

---
+++ Extras/01_Templates/02_Class_Templates/build/CMakeFiles/01_Stack_NoHeader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/01_Stack_NoHeader"
+  "../bin/01_Stack_NoHeader.pdb"
+  "CMakeFiles/01_Stack_NoHeader.dir/examples/01_Stack_NoHeader.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_Stack_NoHeader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Extras/01_Templates/02_Class_Templates/examples/01_Stack_NoHeader.cpp
+++ Extras/01_Templates/02_Class_Templates/examples/01_Stack_NoHeader.cpp
@@ -1,2 +1,60 @@
 #include <vector>
 #include <cassert>
+#include <iostream>
+#include <string>
+
+template <typename T>
+class Stack
+{
+private:
+    std::vector<T> elems; // elements
+public:
+    void push(T const &elem); // push element
+    void pop();               // pop element
+    T const &top() const;     // return top element
+    bool empty() const
+    {
+        return elems.empty();
+    } // return whether the stack is empty
+};
+
+template <typename T>
+void Stack<T>::push(T const &elem)
+{
+    elems.push_back(elem); // append copy of passed elem
+}
+
+template <typename T>
+void Stack<T>::pop()
+{
+    assert(!elems.empty());
+    elems.pop_back(); // remove last element
+}
+
+template <typename T>
+T const &Stack<T>::top() const
+{
+    assert(!elems.empty());
+    return elems.back(); // return copy of last element
+}
+
+int main()
+{
+    Stack<int> intStack;            // stack of ints
+    Stack<std::string> stringStack; // stack of strings
+
+    // manipulate int stack
+    intStack.push(1);
+    intStack.push(2);
+    intStack.push(3);
+    intStack.pop();
+    std::cout << intStack.top() << '\n';
+
+    // manipulate string stack
+    stringStack.push("hello");
+    stringStack.push("world");
+    stringStack.empty();
+    stringStack.push("a");
+    std::cout << stringStack.top() << '\n';
+    stringStack.pop();
+}
