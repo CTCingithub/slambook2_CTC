c70a3d6ce18ba0cd8513a5697dcc64841c2135f1
CTCingithub <2801320287@qq.com>
2025-02-12 22:03:43 +0800

    Updated Ceres Example

--- Chapter06/02_CurveFitting_Ceres/include/Problems.h
+++ Chapter06/02_CurveFitting_Ceres/include/Problems.h
@@ -2,14 +2,22 @@
 
 namespace Problem
 {
-    struct Quad_Minimum
-    // Find minimum of f(x) = \frac{1}{2} (10 - x)^2
+    // 代价函数的计算模型
+    struct CURVE_FITTING_COST
     {
+        CURVE_FITTING_COST(double x, double y) : _x(x), _y(y) {}
+
+        // 残差的计算
         template <typename T>
-        bool operator()(const T *const x, T *residual) const
+        bool operator()(
+            const T *const abc, // 模型参数，有3维
+            T *residual) const
         {
-            residual[0] = 10.0 - x[0];
+            // y-exp(ax^2+bx+c)
+            residual[0] = T(_y) - ceres::exp(abc[0] * T(_x) * T(_x) + abc[1] * T(_x) + abc[2]);
             return true;
         }
+
+        const double _x, _y; // x,y数据
     };
 };
--- Chapter06/02_CurveFitting_Ceres/main.cpp
+++ Chapter06/02_CurveFitting_Ceres/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <filesystem>
-// #include <Eigen/Dense>
+#include <chrono>
 #include <ceres/ceres.h>
+#include "yaml-cpp/yaml.h"
 #include "DataGeneration.h"
 #include "Problems.h"
 
@@ -13,22 +14,35 @@ int main(int argc, char **argv)
     std::string file = std::filesystem::current_path().string() + "/config/example.yaml";
     GenerateData(file, x_data, y_data);
 
+    // Load Configuration
+    YAML::Node node = YAML::LoadFile(file);
+    int num_data = node["NUM_Data"].as<int>();
+    double ae = node["Estimated"]["a"].as<double>();
+    double be = node["Estimated"]["b"].as<double>();
+    double ce = node["Estimated"]["c"].as<double>();
+    double abc[3] = {ae, be, ce};                   // 估计参数值
+
     ceres::Problem problem_1;
-    double initial_x = 5.0;
-    double x = initial_x;
-    ceres::CostFunction *cost_function =
-        new ceres::AutoDiffCostFunction<Problem::Quad_Minimum, 1, 1>(
-            new Problem::Quad_Minimum);
-    problem_1.AddResidualBlock(cost_function, nullptr, &x);
+    for (int i=0;i<num_data;i++){
+        ceres::CostFunction *cost_function =
+            new ceres::AutoDiffCostFunction<Problem::CURVE_FITTING_COST,1,3>(
+                new Problem::CURVE_FITTING_COST(x_data[i], y_data[i])
+            );
+        problem_1.AddResidualBlock(cost_function, nullptr, abc);
+    }
 
     // Run the solver
     ceres::Solver::Options options;
     options.linear_solver_type = ceres::DENSE_QR;
     options.minimizer_progress_to_stdout = true;
+
     ceres::Solver::Summary summary;
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     ceres::Solve(options, &problem_1, &summary);
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    std::cout << "solve time cost = " << time_used.count() << " seconds. " << std::endl;
+
     std::cout << summary.FullReport() << "\n";
-    std::cout << "Initial x: " << initial_x << "\n";
-    std::cout << "Final   x: " << x << "\n";
     return 0;
 }
